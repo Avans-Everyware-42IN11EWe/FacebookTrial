b85aeafacc7ed71e8954735e93894da230c868c5
Nanne Wielinga <nanne@mycel.nl>
2014-03-14 12:20:21 +0100

    aanpassingen!!

--- app/src/main/java/nl/glassaanhuis/facebooktrial/app/ProfileActivity.java
+++ app/src/main/java/nl/glassaanhuis/facebooktrial/app/ProfileActivity.java
@@ -1,9 +1,14 @@
 package nl.glassaanhuis.facebooktrial.app;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,7 +17,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.HttpMethod;
 import com.facebook.NonCachingTokenCachingStrategy;
@@ -35,22 +43,24 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProfileActivity extends Activity {
-
+    private String url;
     void draw(Session session) {
         final TextView name = (TextView) findViewById(R.id.userName);
         final ProfilePictureView pic = (ProfilePictureView) findViewById(R.id.photo);
-        final TextView user = (TextView) findViewById(R.id.textView);
         final String str = session.getAccessToken();
 
         Request.newMeRequest(session, new Request.GraphUserCallback() {
@@ -147,46 +157,82 @@ public class ProfileActivity extends Activity {
             try {
                 JSONObject jObject = new JSONObject(result);
                 JSONArray plaatjes;
+                JSONArray probeer = jObject.getJSONArray("photos");
                 plaatjes = jObject.getJSONArray("plaatjes");
 
                 // doe iets met data
+                url = probeer.getJSONObject(0).get("source").toString();
+
                 GridView gridview = (GridView) findViewById(R.id.gridLayout);
                 gridview.setAdapter(new ImageAdapter(plaatjes, ProfileActivity.this));
+                RelativeLayout viewtje = (RelativeLayout) findViewById(R.id.background);
+                String[] params = new String[2];
+
+                params[0] = url;
+                DownloadImageTask task =  new DownloadImageTask(viewtje);
+                task.execute(params);
+
+
+
 
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-
+        }
 
         }
 
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        RelativeLayout viewtje;
+
+        public DownloadImageTask(RelativeLayout bmImage) {
+            this.viewtje = bmImage;
+        }
+
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            Drawable d = new BitmapDrawable(getResources(),result);
+            viewtje.setBackground(d);
+            viewtje.getBackground().setAlpha(55);
+        }
     }
 
-    public void likePage(View v){
+        public void likePage(View v) {
 
 
-	 //final String urlFb = "fb://page/glasvezelpaleiskwartier";
-     final String urlFb = "fb://page/1450066045229608";
-	 Intent intent = new Intent(Intent.ACTION_VIEW);
-	 intent.setData(Uri.parse(urlFb));
+            //final String urlFb = "fb://page/glasvezelpaleiskwartier";
+            final String urlFb = "fb://page/1450066045229608";
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(urlFb));
 
-     final PackageManager packageManager = getPackageManager();
-	 List<ResolveInfo> list = packageManager.queryIntentActivities(
-	 intent, PackageManager.MATCH_DEFAULT_ONLY);
-	 if (list.size() == 0) {
-         //final String urlBrowser = "https://www.facebook.com/glasvezelpaleiskwartier";
-         final String urlBrowser = "https://www.facebook.com/pages/1450066045229608";
+            final PackageManager packageManager = getPackageManager();
+            List<ResolveInfo> list = packageManager.queryIntentActivities(
+                    intent, PackageManager.MATCH_DEFAULT_ONLY);
+            if (list.size() == 0) {
+                //final String urlBrowser = "https://www.facebook.com/glasvezelpaleiskwartier";
+                final String urlBrowser = "https://www.facebook.com/pages/1450066045229608";
 
-	 intent.setData(Uri.parse(urlBrowser));
+                intent.setData(Uri.parse(urlBrowser));
 
-	 }
+            }
 
-	 startActivity(intent);
+            startActivity(intent);
+
+        }
+
+
 
     }
-
-
-
-
-
-}
